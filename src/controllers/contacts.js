71d8dbb04f6da00e72fe9775dc1796476444ec64
leguter <152528231+leguter@users.noreply.github.com>
2024-11-01 17:19:26 +0300

    done

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -30,3 +30,26 @@ export const addContactController = async(req, res) => {
     })
 
 }
+export const patchContactController = async (req,res,next) => {
+const {id} = req.params;
+const data = await contactServices.updateContact({id,payload:req.body})
+if(!data) {
+    next(createHttpError(404, `Contact with id=${id} not found`))
+    return
+}
+res.json({
+	status: 200,
+	message: "Successfully patched a contact!",
+	data
+	// оновлені дані контакту
+})
+}
+export const deleteContactController = async (req,res) => {
+    const {id} = req.params;
+    const data = await contactServices.deleteContact(id)
+    if(!data) {
+        throw createHttpError(404, `Movie with id=${id} not found`);
+    }
+
+    res.status(204).send();
+}
--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -3,9 +3,11 @@ import { Router } from "express";
 // import * as contactServices from '../services/contacts.js'
 const router = Router();
 import { ctrlWrapper } from "../utils/ctrlWrapper.js";
-import { getAllContactsController, getContactByIdController,addContactController} from "../controllers/contacts.js";
+import { getAllContactsController, getContactByIdController,addContactController, patchContactController, deleteContactController} from "../controllers/contacts.js";
    
 router.get('/contacts', ctrlWrapper(getAllContactsController))
 router.get('/contacts/:id', ctrlWrapper(getContactByIdController))
 router.post('/contacts',ctrlWrapper(addContactController))
+router.patch('/contacts/:id', ctrlWrapper(patchContactController))
+router.delete('/contacts/:id', ctrlWrapper(deleteContactController))
 export default router;
