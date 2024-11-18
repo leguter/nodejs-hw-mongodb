dfeeed6d6d8dff4522b736c650b5d9fb23a9ec72
leguter <152528231+leguter@users.noreply.github.com>
2024-11-18 07:45:40 +0700

    fix 2.0

---
+++ src/utils/parseFilterParams.js
@@ -0,0 +1,5 @@
+
+const parseType = (type) => {
+    const isString = typeof type === "string";
+    if (!type) return
+}
--- src/utils/validateBody.js
+++ src/utils/validateBody.js
@@ -6,7 +6,7 @@ try {
     })
     next()
 } catch(err) {
-    const error = createHttpError(404, "Bad request", {
+    const error = createHttpError(404, "Some field(/s) do not right or missing", {
         errors: err.details,
     })
     next(error)
