39b3719033c903875cab3d8448e024db4b71f972
leguter <152528231+leguter@users.noreply.github.com>
2024-11-18 11:14:57 +0700

    3

--- src/models/modelSchema.js
+++ src/models/modelSchema.js
@@ -2,11 +2,16 @@
 import { Schema, model } from "mongoose";
 const userSchema = new Schema({
     name: {type:String, required: true},
-    email: {type: String, required: true, unique: true, email},
+    email: {type: String, required: true, unique: true},
     password:  {type:String, required: true},
     createdAt: {},
     updatedAt: {},
 },
 {timestamps: true, versionKey: false})
+userSchema.methods.toJSON = function () {
+    const obj = this.toObject();
+    delete obj.password;
+    return obj;
+}
 const UserCollection = model('users', userSchema)
 export default UserCollection;
--- src/services/auth.js
+++ src/services/auth.js
@@ -1,4 +1,10 @@
-import { UserCollection } from "../models/modelSchema.js";
+import bcrypt from "bcrypt"
+import UserCollection  from "../models/modelSchema.js";
+import createHttpError from "http-errors";
 export const registerUser = async( payload) => {
-return await UserCollection.create(payload)
+    const email = UserCollection.findOne({email: payload.email})
+    if (email) throw createHttpError(409,"Email in use")
+        // problem with email
+    const encryptedPassword = await  bcrypt.hash(payload.password,10 )
+return await UserCollection.create({...payload, password: encryptedPassword})
 }
